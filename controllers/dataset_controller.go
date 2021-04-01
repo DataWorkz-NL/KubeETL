968402fd508cabdc159799779ffebaa4a1f7dbcb
Lennard Eijsackers <lennardeijsackers92@gmail.com>
2021-04-01 16:00:31 +0200

    Feat: List cronjobs

--- controllers/dataset_controller.go
+++ controllers/dataset_controller.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/go-logr/logr"
 	batch "k8s.io/api/batch/v1beta1"
@@ -35,6 +36,12 @@ func (r *DataSetReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ct
 		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
 
+	var cronJobs batch.CronJobList
+	if err := r.List(ctx, &cronJobs, client.InNamespace(req.Namespace), client.MatchingFields{".metadata.controller": req.Name}); err != nil {
+		log.Error(err, "unable to list CronJobs")
+		return ctrl.Result{}, err
+	}
+
 	cronJob := DataSetToCronJob(dataSet)
 	if err := r.TypedMutator.MutateCronJob(ctx, &dataSet, &cronJob); err != nil {
 		log.Error(err, "Could not create CronJob for DataSet HealthCheck")
@@ -66,6 +73,22 @@ func DataSetToCronJob(dataSet v1alpha1.DataSet) batch.CronJob {
 func (r *DataSetReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	if r.TypedMutator == nil {
 		r.TypedMutator = mutators.New(r.Client, r.Scheme, r.Log)
+	}
+
+	if err := mgr.GetFieldIndexer().IndexField(context.Background(), &batch.CronJob{}, ".metadata.controller", func(rawObj client.Object) []string {
+		job := rawObj.(*batch.CronJob)
+		owner := metav1.GetControllerOf(job)
+		if owner == nil {
+			return nil
+		}
+
+		if owner.APIVersion != api.GroupVersion.String() || owner.Kind != "DataSet" {
+			return nil
+		}
+
+		return []string{owner.Name}
+	}); err != nil {
+		return fmt.Errorf("failed to set up index on CronJob: %w", err)
 	}
 
 	return ctrl.NewControllerManagedBy(mgr).
