db0848010fb2dd46d00c6133981ade35fa310e55
Lennard Eijsackers <lennardeijsackers92@gmail.com>
2022-01-11 19:28:58 +0100

    Unify ConnectionProvider & Manager into single CLI. (#52)
    Creating a single unified CLI for KubeETL to handle managing both the Manager & the ConnectionProvider, to prevent a wild growth of docker images
    
    Signed-off-by: Lennard Eijsackers <lennardeijsackers92@gmail.com>

--- main.go
+++ main.go
@@ -1,101 +1,13 @@
-/*
-
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package main
 
 import (
-	"flag"
 	"os"
 
-	"k8s.io/apimachinery/pkg/runtime"
-	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
-	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
-	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/log/zap"
-
-	etldataworkznlv1alpha1 "github.com/dataworkz/kubeetl/api/v1alpha1"
-	etlv1alpha1 "github.com/dataworkz/kubeetl/api/v1alpha1"
-	etlhooks "github.com/dataworkz/kubeetl/api/v1alpha1/webhooks"
-	"github.com/dataworkz/kubeetl/controllers"
-	// +kubebuilder:scaffold:imports
+	"github.com/dataworkz/kubeetl/internal/commands"
 )
-
-var (
-	scheme   = runtime.NewScheme()
-	setupLog = ctrl.Log.WithName("setup")
-)
-
-func init() {
-	_ = clientgoscheme.AddToScheme(scheme)
-
-	_ = etlv1alpha1.AddToScheme(scheme)
-	_ = etldataworkznlv1alpha1.AddToScheme(scheme)
-	// +kubebuilder:scaffold:scheme
-}
 
 func main() {
-	var metricsAddr string
-	var enableLeaderElection bool
-	var connectionInjectionImage string
-	flag.StringVar(&metricsAddr, "metrics-addr", ":8080", "The address the metric endpoint binds to.")
-	flag.BoolVar(&enableLeaderElection, "enable-leader-election", false,
-		"Enable leader election for controller manager. "+
-			"Enabling this will ensure there is only one active controller manager.")
-	flag.StringVar(&connectionInjectionImage, "connection-injection-image", "dataworkz/kubeetl-injector", "The image that will provide connection injection functionality for Workflows")
-	flag.Parse()
-
-	ctrl.SetLogger(zap.New(zap.UseDevMode(true)))
-	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
-		Scheme:             scheme,
-		MetricsBindAddress: metricsAddr,
-		Port:               9443,
-		LeaderElection:     enableLeaderElection,
-		LeaderElectionID:   "1345a080.dataworkz.nl",
-	})
-	if err != nil {
-		setupLog.Error(err, "unable to start manager")
-		os.Exit(1)
-	}
-
-	if err = (&controllers.DataSetReconciler{
-		Client: mgr.GetClient(),
-	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "DataSet")
-		os.Exit(1)
-	}
-
-	err = etlhooks.SetupValidatingConnectionWebhookWithManager(mgr)
-	if err != nil {
-		setupLog.Error(err, "unable to start webhook")
-		os.Exit(1)
-	}
-	if err = (&controllers.WorkflowReconciler{
-		Client:                   mgr.GetClient(),
-		Log:                      ctrl.Log.WithName("controllers").WithName("Workflow"),
-		Scheme:                   mgr.GetScheme(),
-		ConnectionInjectionImage: connectionInjectionImage,
-	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "Workflow")
-		os.Exit(1)
-	}
-	// +kubebuilder:scaffold:builder
-
-	setupLog.Info("starting manager")
-	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
-		setupLog.Error(err, "problem running manager")
+	if err := commands.NewRootCommand().Execute(); err != nil {
 		os.Exit(1)
 	}
 }
