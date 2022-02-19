e5beff3dfd94006cc660071563f34a5499fff22a
Lennard Eijsackers <lennardeijsackers92@gmail.com>
2022-02-19 21:32:45 +0100

    feat: add schema registration to ControllerManager

--- pkg/manager/manager.go
+++ pkg/manager/manager.go
@@ -1,6 +1,10 @@
 package manager
 
-import "k8s.io/apimachinery/pkg/runtime"
+import (
+	"fmt"
+
+	"k8s.io/apimachinery/pkg/runtime"
+)
 
 // ControllerManager manages all of KubeETLs controllers + webhooks
 type ControllerManager struct {
@@ -12,15 +16,30 @@ type ControllerManager struct {
 	metricsAddr string
 	
 	scheme *runtime.Scheme
+	schemasRegistration []SchemeRegistration
 }
 
 func New(opts ...ControllerManagerOpts) *ControllerManager {
+	// TODO set defaults
 	m := &ControllerManager{}
 	for _, opt := range opts {
 		opt(m)
 	}
 
 	return m
+}
+
+// Init initializes all components of the KubeETL operator:
+// - It adds the KubeETL schemas to the runtime.Scheme
+func (cm *ControllerManager) Init() error {
+	for _, addToScheme := range cm.schemasRegistration {
+		err := addToScheme(cm.scheme)
+		if err != nil {
+			return fmt.Errorf("could not initialize schemas: %w", err)
+		}
+	}
+
+	return nil
 }
 
 type ControllerManagerOpts func(*ControllerManager)
@@ -55,5 +74,13 @@ func WithScheme(scheme *runtime.Scheme) ControllerManagerOpts {
 func WithDefaultScheme() ControllerManagerOpts {
 	return func(cm *ControllerManager) {
 		cm.scheme = runtime.NewScheme()
+	}
+}
+
+type SchemeRegistration func(*runtime.Scheme) error
+
+func WithSchemas(schemas ...SchemeRegistration) ControllerManagerOpts {
+	return func(cm *ControllerManager) {
+		cm.schemasRegistration = schemas
 	}
 }
